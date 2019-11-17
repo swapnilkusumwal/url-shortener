73f4f716fc2afeef05e139d7ab62bc8f4bbffa79
Glitch (flower-silicon) <none>
2019-11-17 10:52:37 +0000

    👸🚔 Checkpoint ./server.js:3860245/5442

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-'use strict';
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');  
+"use strict";
+var express = require("express");
+var mongo = require("mongodb");
+var mongoose = require("mongoose");
 //var shortid = require('shortid');
 //shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$&');
-var validUrl = require('valid-url');
-var count=0;
-var cors = require('cors');
+var validUrl = require("valid-url");
+var count = 0;
+var cors = require("cors");
 
 var app = express();
 
-// Basic Configuration 
+// Basic Configuration
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
+/** this project needs a db !! **/
+
 // mongoose.connect(process.env.MONGOLAB_URI);
 
 app.use(cors());
@@ -21,79 +22,74 @@ app.use(cors());
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.post("/api/shorturl/new/:url(*)",function(req,res){
-  mongoose.connect(process.env.MONGO_URL,{ useNewUrlParser: true},function(err,db){
-    if(err){
+app.post("/api/shorturl/new/:url(*)", function(req, res) {
+  mongoose.connect(process.env.MONGO_URL, { useUnifiedTopology: true }, function(err,db) {
+    if (err) {
       console.log("Error in connecting to database");
-    }
-    else
-      {
-        let url=req.params.url;
-        
-        if(validUrl.isUri(url)){
-       
-          let collections=db.collections('links');
-          var obj={
-            original_url:url,
-            short:"https://url-shortener99.glitch.me/"+count.toString()
-          };
-          count++;
-          collections.insert(obj,function(err,data){
-            if(err)
-              {
-                console.log("error inserting in database");
-              }
-            else
-              db.close();
-          });
-        }
-        else
-          {
-            res.send({
-              error:"The URL format is wrong kindly recheck the url and POST again"
-            });
-          }
-      }
-  });
-});
+    } else {
+      let url = req.params.url;
 
-app.post("/api/shorturl/:now",function(req,res){
-  mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true},function(err,db){
-     if(err){
-       console.log("cannot connect to database second time");
-     }
-    else
-      {
-        let collections=db.collections("links");
-        let val=req.params.now;
+      if (validUrl.isUri(url)) {
+        let collections = db.collection("links");
+        var obj = {
+          original_url: url,
+          short: "https://url-shortener99.glitch.me/" + count.toString()
+        };
+        count++;
+        res.send(JSON.stringify(obj));
         
-        collections.findOne({short:"https://url-shortener99.glitch.me/"+val.toString()},function(err,data){
-          if(data!=null){
-            res.redirect(data.original_url);
+        collections.insert(obj, function(err, data) {
+          if (err) {
+            console.log("error inserting in database");
           }
-          else
-            {
-              console.log("short url not found in database");
-            }
           db.close();
         });
+      } else {
+        res.send({
+          error: "The URL format is wrong kindly recheck the url and POST again"
+        });
       }
+    }
   });
 });
 
+app.post("/api/shorturl/:now", function(req, res) {
+  mongoose.connect(process.env.MONGO_URL, { useUnifiedTopology: true }, function(
+    err,
+    db
+  ) {
+    if (err) {
+      console.log("cannot connect to database second time");
+    } else {
+      let collections = db.collection("links");
+      let val = req.params.now;
 
-app.listen(port, function () {
-  console.log('Node.js listening ...');
+      collections.findOne(
+        { short: "https://url-shortener99.glitch.me/" + val.toString() },
+        function(err, data) {
+          if (data != null) {
+            res.redirect(data.original_url);
+          } else {
+            console.log("short url not found in database");
+          }
+          db.close();
+        }
+      );
+    }
+  });
+});
+
+app.listen(port, function() {
+  console.log("Node.js listening ...");
 });
